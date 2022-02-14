413f40de27a7eeff5a6744db2d1941741f5ffe27
AlCh440 <albert.chica@estudiantat.upc.edu>
2022-02-14 21:08:10 +0100

    creating tsx

---
+++ Platformer/Output/Assets/maps/art.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="art" tilewidth="64" tileheight="64" tilecount="40" columns="8">
+ <image source="soldor_tileset_lila.png" width="534" height="371"/>
+</tileset>
---
+++ Platformer/Output/Assets/maps/soldor_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="soldor_tileset" tilewidth="64" tileheight="64" tilecount="77" columns="11">
+ <image source="soldor_tileset.png" width="704" height="448"/>
+</tileset>
